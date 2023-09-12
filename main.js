4751e4668f4b5e752f3d31eb72baad8c7defc1f7
Sachin Kumar <96949859+Sachin-Dashzer@users.noreply.github.com>
2023-09-13 01:24:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 
-gsap.to('nav', {
-
-    y: 100,
+gsap.from('nav', {
+    y : -100,
+    opacity: 0,
     duration: 1,
-    opacity: 1
 })
 
 
@@ -12,22 +11,22 @@ const tl = gsap.timeline();
 tl.from(" header section h1" , {
     x : -200,
     duration:1,
-    delay:1,
+    stagger : 0.5,
     opacity:0
 })
 tl.from(" header section h2" , {
     x : -200,
-    duration:1,
+    duration:0.8,
     opacity:0
 })
 tl.from("header section p" , {
     x : -200,
-    duration:0.8,
+    duration:0.6,
     opacity:0
 })
 tl.from("header section button" , {
     x : -200,
-    duration:0.6,
+    duration:0.4,
     opacity:0
 })
 
@@ -35,7 +34,7 @@ tl.from("header section button" , {
 
 gsap.from("#about", {
 
-    y: 200,
+    y: 50,
     opacity: 0,
     duration: 1.5,
 
@@ -53,28 +52,9 @@ gsap.from("#about", {
 
 
 
-// gsap.from(".lol", {
-
-//     y: 200,
-//     opacity: 0,
-//     duration: 1.5,
-
-//     scrollTrigger: {
-//         trigger: ".lol",
-//         scroller: "body",
-//         // markers:true,
-//         // start:"top 50%",
-//         // end:"top 30%",
-
-//     }
-
-
-// })
-
-
 gsap.from("#shop", {
 
-    y: 100,
+    y: 50,
     opacity: 0,
     duration: 1.5,
 
@@ -92,7 +72,7 @@ gsap.from("#shop", {
 
 gsap.from(".box1", {
 
-    x: 200,
+    x: 100,
     opacity: 0,
     duration: 1.5,
     scrollTrigger: {
@@ -119,17 +99,38 @@ gsap.from(".box1", {
 //     }
 // })
 
-// gsap.from("footer", {
 
-//     y: 200,
-//     opacity: 0,
-//     duration: 1.5,
-//     scrollTrigger: {
-//         trigger: "footer",
-//         scroller: "body",
-//         // markers:true,
-//         // start:"top 50%",
-//         // end:"top 30%",
 
-//     }
-// })
+let menu = document.querySelector("#menubar");
+let menuBtn = document.querySelector("#menubutton");
+
+let ans = false;
+
+
+menuBtn.addEventListener("click" , function(){
+
+    ans = !ans;
+
+
+    if(ans == true){
+
+    gsap.to(menu , {
+        right : 0,
+        opacity : 1,
+        duration : 1,
+        
+
+    })}
+
+    else{
+        gsap.to(menu , {
+            right : "-23%",
+            opacity : 0,
+            duration : 1,
+            
+    
+        })
+    
+    }
+
+});
